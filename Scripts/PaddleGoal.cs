f5a3679ac63b57707d90e05241679f1d5ed08ff0
ManavGhaiCode <manavghai12october@gmail.com>
2023-07-15 16:38:25 +0530

    add Win Screen

--- Scripts/PaddleGoal.cs
+++ Scripts/PaddleGoal.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PaddleGoal : MonoBehaviour {
+    public TextController winStr;
     public TextController txt;
+    public Transform win;
 
-    int Score = 0;
+    public int playerNum;
+
+    int Score = 9;
 
     private void OnTriggerEnter2D(Collider2D hitInfo) {
         Ball ball = hitInfo.GetComponent<Ball>();
@@ -17,9 +21,22 @@ public class PaddleGoal : MonoBehaviour {
             Score += 1;
             txt.SetText(Score.ToString());
 
-            if (Score > 10) {
-                SceneManager.LoadScene("Game");
+            if (Score >= 10) {
+                win.position = new Vector3 (0, 10, 0);
+
+                winStr.SetText("Player " + playerNum.ToString());
+
+                foreach (Rigidbody2D rb in GameObject.FindObjectsOfType<Rigidbody2D>()) {
+                    rb.bodyType = RigidbodyType2D.Static;
+                }
+
+                StartCoroutine(ResetScene());
             }
         }
+    }
+
+    private IEnumerator ResetScene() {
+        yield return new WaitForSeconds (2f);
+        // SceneManager.LoadScene("Game");
     }
 }
